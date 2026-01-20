561404542c76e22b554a55442b54cb38c6a07858
Aditya Porwal <adityaporwal3251@gmail.com>
2026-01-20 18:30:31 +0530

    connect DB , cloudinary and test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import app from './app.js'
 dotenv.config({
     path: './.env'
 })
@@ -7,4 +8,11 @@ dotenv.config({
 
 
 connectDB()
-
+.then(() => {
+    app.listen(process.env.PORT || 7000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
