7dcb24f2329bd5f8ba62b2802aa130c03c5eb78a
Aditya Porwal <adityaporwal3251@gmail.com>
2026-01-22 15:07:18 +0530

    update account details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -241,4 +241,19 @@ const getCurrentUser = asyncHandler(async(req,res)=>{
     return res.status(200)
     .json(200,req.user,"user found successfully")
 })
-export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser}
+
+const updateAccountDetails = asyncHandler(async(req,res)=>{
+    const {fullName,email} = req.body
+    if(!fullName&&!email){
+        throw new ApiError(400,"fullname or email must be present")
+    }
+    const user = await User.findByIdAndUpdate(req.user?._id,
+        {$set:{
+         fullName,
+         email:email
+         }}
+    ,{new:true}).select("-password")
+    return res.status(200)
+    .json(new ApiResponse(200,user,"Account details updated successfully"))
+})
+export {registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateAccountDetails}
