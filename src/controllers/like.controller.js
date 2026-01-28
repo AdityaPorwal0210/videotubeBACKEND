6d14b2eac66078e5a12e3cc03729354e7cf1f1f3
Aditya Porwal <adityaporwal3251@gmail.com>
2026-01-28 12:05:05 +0530

    like controller

---
+++ src/controllers/like.controller.js
@@ -0,0 +1,128 @@
+import mongoose from "mongoose";
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse";
+import { Like } from "../models/like.model";
+
+const toogleVideoLike = asyncHandler(async(req,res)=>{
+    //verify user
+    // if found one then delete 
+    // create like
+    const{videoId} = req.params
+    const userId = req.user?._id
+    if(!videoId){
+        throw new ApiError(401,"videoId is invalid")
+    }
+    if(!userId){
+        throw new ApiError(401,"userId is invalid")
+    }
+
+    const deletedLike = await Like.findOneAndDelete(
+        {video:videoId,likedBy:userId}
+    )
+    if(deletedLike){
+        return res.status(200).json(new ApiResponse(200,null,"unliked successfully"))
+    }
+
+    const newLike = await Like.create({video:videoId,likedBy:userId})
+    if(!newLike){
+        throw new ApiError(405,"error while liking this video")
+    }
+    return res.status(200).json(new ApiResponse(200,newLike,"liked this video successfully"))
+})
+const toogleCommentLike = asyncHandler(async(req,res)=>{
+    //verify user
+    // if found one then delete 
+    // create like
+    const{commentId} = req.params
+    const userId = req.user?._id
+    if(!commentId){
+        throw new ApiError(401,"commentId is invalid")
+    }
+    if(!userId){
+        throw new ApiError(401,"userId is invalid")
+    }
+
+    const deletedLike = await Like.findOneAndDelete(
+        {comment:commentId,likedBy:userId}
+    )
+    if(deletedLike){
+        return res.status(200).json(new ApiResponse(200,null,"unliked successfully"))
+    }
+
+    const newLike = await Like.create({comment:commentId,likedBy:userId})
+    if(!newLike){
+        throw new ApiError(405,"error while liking this video")
+    }
+    return res.status(200).json(new ApiResponse(200,newLike,"liked this video successfully"))
+})
+
+const toogleTweetLike = asyncHandler(async(req,res)=>{
+    //verify user
+    // if found one then delete 
+    // create like
+    const{TweetId} = req.params
+    const userId = req.user?._id
+    if(!TweetId){
+        throw new ApiError(401,"TweetId is invalid")
+    }
+    if(!userId){
+        throw new ApiError(401,"userId is invalid")
+    }
+
+    const deletedLike = await Like.findOneAndDelete(
+        {tweet:TweetId,likedBy:userId}
+    )
+    if(deletedLike){
+        return res.status(200).json(new ApiResponse(200,null,"unliked successfully"))
+    }
+
+    const newLike = await Like.create({tweet:TweetId,likedBy:userId})
+    if(!newLike){
+        throw new ApiError(405,"error while liking this video")
+    }
+    return res.status(200).json(new ApiResponse(200,newLike,"liked this video successfully"))
+})
+
+const getLikedVideos = asyncHandler(async(req,res)=>{
+    //verify user
+
+    const userId = req.user?._id
+    if (!mongoose.Types.ObjectId.isValid(userId)) {
+        throw new ApiError(400, "Invalid User ID");
+    }
+
+    const likedVideos = await Like.aggregate([
+        {
+            $match:{
+                likedBy: new mongoose.Types.ObjectId(userId),
+                video: { $exists: true,$ne : null},
+            },
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"video",
+                foreignField:"_id",
+                as:"videoDetails",
+                pipeline:[
+                    {
+                        $project:{
+                            videoFile:1,
+                            thumbnail:1,
+                            title:1,
+                            owner:1
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+    if(!likedVideos){
+        throw new ApiError(405,"error while fetching liked videos")
+    }
+    return res.status(200).json(new ApiResponse(200,likedVideos,"fetched liked videos successfully"))
+    
+})
+
+export {getLikedVideos,toogleCommentLike,toogleTweetLike,toogleVideoLike}
